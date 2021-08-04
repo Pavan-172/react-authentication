e8418cc37fb7be6a37d9f196cf8a836666793f07
Pavan-172 <p2pavankumar17@gmail.com>
2021-08-04 08:59:48 +0530

    1 file modified

--- src/App/interaction.js
+++ src/App/interaction.js
@@ -8,6 +8,7 @@ export const login = (email,password)=>{
     return axios
     .post(`${BASE_URL}/login`,{email,password})
     .then((res)=>res.data)
+   // console.log(res.data)
     .catch((error)=>(error.response.data))
 }
 
@@ -15,5 +16,6 @@ export const signup=(email,firstname,lastname,password)=>{
     return axios
     .post(`${BASE_URL}/register`,{email,firstname,lastname,password})
     .then((res)=>res.data)
+    //console.log(res.data)
     .catch((error)=>(error.response.data));
 }
